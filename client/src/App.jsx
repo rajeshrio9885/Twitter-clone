73b95f929f13be571afb60fa61e0a258bd14e335
rajeshrio9885 <nirmalvicky62@gmail.com>
2024-12-09 14:05:44 +0530

    simple

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -60,7 +60,6 @@ const App = () => {
       <Route path='/notifications' element={authUser ? <NotificationPage/> : <Navigate to={"/login"}/>}/>
       <Route path='/profile/:username' element={authUser?<ProfilePage/> : <Navigate to={"/login"}/>}/>
       <Route path='/signup' element={!authUser ? <SignUpPage/> :<Navigate to={"/"}/>}/>
-      <Route path="*" element={!authUser?<LoginPage/>: <Navigate to={"/"}/> } />
      </Routes>
      {authUser && <RightPanel/>}
     </div>
